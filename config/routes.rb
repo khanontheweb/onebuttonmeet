fc1bdd3b44e27ee496127259dd54563ca40cbd23
Momo Khan <snillmomo@gmail.com>
2019-01-28 11:05:37 -0500

    Create Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/create'
+  get 'users/read'
+  get 'users/update'
+  get 'users/destroy'
   get 'sessions/create'
   get 'sessions/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
