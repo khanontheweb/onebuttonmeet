6f5e58638a2368bd358190f9faf73949d25b140f
Momo Khan <snillmomo@gmail.com>
2019-01-29 13:48:39 -0500

    Add user home page and map functionality

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,42 @@
+var map, infoWindow;
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 18,
+        center: {lat: -34.397, lng: 150.644},
+        mapTypeControl: false,
+        streetViewControl: false
+    });
+
+    if(navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: map
+            });
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('You are here!');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    }
+
+    else {
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+                           'Error: Geolocation failed.' :
+                            'Erro: Browser does not support geolocation.');
+    infoWindow.open(map);
+}
