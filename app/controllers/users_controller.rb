87a4258f3eb3405418b22471ec841fa65f8e68c8
Momo Khan <snillmomo@gmail.com>
2019-01-29 16:02:05 -0500

    Add shadows and borders to buttons and larger elements. Change bg color. Setup event views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def index
   end
 
+
   def create
     @user = User.new(user_params)
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_28_180429) do
+ActiveRecord::Schema.define(version: 2019_01_29_200332) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_01_28_180429) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "live"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
